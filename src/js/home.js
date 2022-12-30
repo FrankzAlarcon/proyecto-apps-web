9682dd7c590dcc5f2f4bdfe33b943919a954f85e
Frankz <leninalarcon7@gmail.com>
2022-12-30 10:34:11 -0500

    home page UI v1 - finished

--- src/js/home.js
+++ src/js/home.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function initApp() {
   renderServices()
+  optionsNavigation()
 }
 
 function renderServices() {
@@ -25,6 +26,45 @@ function renderServices() {
     container.appendChild(price)
     servicesContainer.appendChild(container)
   })
+}
 
-  
+function optionsNavigation() {
+  const serviceButton = document.querySelector(".service-button")
+  const informationButton = document.querySelector(".information-button")
+  const summaryButton = document.querySelector(".summary-button")
+
+  const servicesContainer = document.querySelector(".services-container")
+  const informationContainer = document.querySelector(".information-container")
+  const summaryContainer = document.querySelector(".summary-container")
+
+  const showContainer = (show, hidden) => {
+    show.classList.remove("hidden")
+
+    hidden.forEach(container => {
+      container.classList.add("hidden")
+    })
+  }
+
+  const setButtonStyles = (selected, others) => {
+    selected.classList.add("selected")
+    
+    others.forEach(button => {
+      button.classList.remove("selected")
+    })
+  }
+
+  serviceButton.addEventListener("click", () => {
+    setButtonStyles(serviceButton, [informationButton, summaryButton])
+    showContainer(servicesContainer, [informationContainer, summaryContainer])
+  })
+
+  informationButton.addEventListener("click", () => {
+    setButtonStyles(informationButton, [serviceButton, summaryButton])
+    showContainer(informationContainer, [servicesContainer, summaryContainer])
+  })
+
+  summaryButton.addEventListener("click", () => {
+    setButtonStyles(summaryButton,[ serviceButton, informationButton])
+    showContainer(summaryContainer, [informationContainer, servicesContainer])
+  })
 }
